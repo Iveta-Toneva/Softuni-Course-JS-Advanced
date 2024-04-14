5b078b1e80012f7a73053da658d8c1585da014a3
Iveta-Toneva <iveta.v.toneva@gmail.com>
2024-04-14 21:28:08 +0100

    Syntax, Functions and Statements

---
+++ Syntax, Functions and Statements/Lab/3-largest-number.js
@@ -0,0 +1,7 @@
+function printLargestNumber(firstNumber, secondNumber, thirdNumber) {
+
+    let result = Math.max(firstNumber, secondNumber, thirdNumber);
+    console.log(`The largest number is ${result}.`);
+
+}
+printLargestNumber(5, -3, 16);
