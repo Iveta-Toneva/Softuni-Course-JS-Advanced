f8a30fd8e1ef6d54247be1c93cc2f3a28307adea
Iveta-Toneva <iveta.v.toneva@gmail.com>
2024-05-09 10:08:52 +0100

    DOM Introduction

---
+++ DOM Introduction/Lab/04.Show More/4-show-more.js
@@ -0,0 +1,8 @@
+function showText() {
+
+    let textElement = document.getElementById('text');
+    textElement.style.display = 'inline';
+    let showElement = document.getElementById('more');
+    showElement.style.display = 'none';
+
+}
