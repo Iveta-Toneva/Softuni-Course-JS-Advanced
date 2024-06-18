66198aa2acb1842093b8e4ade215b89cf18eb75a
Iveta-Toneva <iveta.v.toneva@gmail.com>
2024-06-18 17:52:39 +0100

    Prototypes and Inheritance

---
+++ Prototypes and Inheritance/Lab/1-person.js
@@ -0,0 +1,22 @@
+class Person {
+
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    set fullName(value) {
+
+        let pattern = /[a-zA-Z]+ [a-zA-Z]+/;
+        if (pattern.test(value)) {
+            let [firstName, lastName] = value.split(' ');
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+
+}
